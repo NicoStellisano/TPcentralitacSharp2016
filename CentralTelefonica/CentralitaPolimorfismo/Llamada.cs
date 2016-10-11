d5a30ea15a4aca7de0153b072666048cb253c87e
unknown <alumno@LAB02PC13.labs.extension>
2016-10-11 11:48:14 -0300

    Act

---
+++ CentralTelefonica/CentralitaPolimorfismo/Llamada.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralitaPolimorfismo
+{
+    public abstract class Llamada
+    {
+        private float _duracion;
+
+        protected float Duracion
+        {
+            get { return _duracion; }
+        }
+        private string _nroDestino;
+
+        protected string NroDestino
+        {
+            get { return _nroDestino; }
+        }
+        private string _nroOrigen;
+
+        protected string NroOrigen
+        {
+            get { return _nroOrigen; }
+        }
+
+        public float CostoLlamada
+        {
+            get
+            {
+                
+            }
+        }
+
+
+    }
+}
